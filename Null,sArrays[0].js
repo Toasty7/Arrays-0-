b8dda897a2caa591ef9ade29a8b99716b9fd31af
Toasty7 <143848640+Toasty7@users.noreply.github.com>
2024-01-30 13:58:59 -0800

    Beunas notas

---
+++ Null,sArrays[0].js
@@ -0,0 +1,31 @@
+///used for storing data is a foundational concept///
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn Kubernetes'];
+
+///arrays make javascript lists
+
+///stores any type off information/ any data type 
+
+/// to make an an array directly define its values 
+
+///every value is divided by a coma
+
+/// ' example ,element' , 10, true 
+const hobbies = [' Skateboarding', 'Read more', 'playing videogames'];
+
+console.log(hobbies)
+
+
+///each element has an array with a numbered positon. starting from 0 ,rather than one. 0 is the first item
+
+let li = hobbies [2]
+console.log(2)
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+
+console.log(seasons);
+
+newYearsResolutions [0] = 'Start drawing'
+console.log()
